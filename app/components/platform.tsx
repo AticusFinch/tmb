32ab74c7bca508a72ff381ba9b212869ec40beb7
Stasa Bastrica <stasabastrica7@gmail.com>
2025-11-27 15:05:25 +0100

    platform section

---
+++ app/components/platform.tsx
@@ -0,0 +1,51 @@
+import styles from "./platform.module.css";
+import Container from "./container";
+import Image from "next/image";
+import Button from "./button";
+
+const Platform = () => {
+  return (
+    <section className={styles.platform}>
+      <div className={styles.platformImageContainer}>
+        <Image
+          src="/platform/6.jpg"
+          alt="Platform"
+          width={1920}
+          height={1080}
+          className={styles.platformImage}
+        />
+      </div>
+      <div className={styles.platformContent}>
+        <h1 className={styles.platformTitle}>
+          Trans
+          <br />
+          <span>feministička</span>
+          <br /> <span className={styles.platformTitleSpan}>Platforma</span>
+        </h1>
+        <h3 className={styles.platformSubtitle}>
+          S velikim uzbuđenjem predstavljamo šesto izdanje magazina
+          Transfeministička platforma, koje istražuje temu Transfeminizam:
+          značaj građenja savezništva.
+        </h3>
+        <p className={styles.platformText}>
+          Šta vas čeka u ovom broju? Rubrika Novosti donosi aktuelne informacije
+          o važnim događajima iz regiona vezanim za TIRV teme, feminizam i
+          aktivizam.
+        </p>
+        <div className={styles.platformExtra}>
+          <Button href="/" className={styles.platformExtraButton}>
+            Saznaj više
+          </Button>
+          <span className={styles.platformExtraDate}>27.11.2025.</span>
+          <div className={styles.platformExtraHashTags}>
+            <span>#TDoR2024</span>
+            <span>#TIRV</span>
+            <span>#Feminizam</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Platform;
--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 import NewsSection from "./components/news-section";
 import Container from "./components/container";
+import Platform from "./components/platform";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <NewsSection />
+      <Platform />
       <Footer />
     </>
   );
